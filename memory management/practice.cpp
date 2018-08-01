cd45d394d4fcd00212b9ed05a123e407380940f0
FYR666 <31131580+FYR666@users.noreply.github.com>
2018-08-01 18:56:05 +0800

    second commit
    申请堆内存

--- memory management/practice.cpp
+++ memory management/practice.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 int main(void)
 {
-	int *p = new int;
+	int *p = new int[1000];
 	if (NULL == p)
 	{
 		system("pause");
 		return 0;
 	}
-	*p = 20;
-	cout << *p << endl;
-	delete p;
+	p[0] = 10;
+	p[1] = 20;
+	cout << p[0]<<"," << endl;
+	delete []p;
 	p = NULL;
 	system("pause");
 	return 0;
